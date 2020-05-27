89125cbd2cb018ef95231575fb4db5b5e6e890db
hydrograph <>
2020-05-27 17:12:33 -0400

    {/src/main/java/com/rus_cns_mon/pub_rus_cns_all_bre_data_rule_editing/data_bus_dim.java}

--- src/main/java/com/rus_cns_mon/pub_rus_cns_all_bre_data_rule_editing/data_bus_dim.java
+++ src/main/java/com/rus_cns_mon/pub_rus_cns_all_bre_data_rule_editing/data_bus_dim.java
@@ -6,12 +6,133 @@ package com.rus_cns_mon.pub_rus_cns_all_bre_data_rule_editing;
 
 public class data_bus_dim implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public data_bus_dim() {
-    }
+	private java.lang.String dim_name;
+	private java.lang.Long dim_levels__level_0;
+	private java.lang.Long dim_levels__level_1;
+	private java.lang.Long dim_levels__level_2;
+	private java.lang.Long dim_levels__level_3;
+	private java.lang.Long dim_levels__level_4;
+	private java.lang.Long dim_levels__level_5;
+	private java.lang.Long dim_levels__level_6;
+	private java.lang.Long dim_levels__level_7;
+	private java.lang.Long dim_levels__level_8;
+	private java.lang.Long dim_levels__level_9;
 
+	public data_bus_dim() {
+	}
 
+	public java.lang.String getDim_name() {
+		return this.dim_name;
+	}
 
+	public void setDim_name(java.lang.String dim_name) {
+		this.dim_name = dim_name;
+	}
+
+	public java.lang.Long getDim_levels__level_0() {
+		return this.dim_levels__level_0;
+	}
+
+	public void setDim_levels__level_0(java.lang.Long dim_levels__level_0) {
+		this.dim_levels__level_0 = dim_levels__level_0;
+	}
+
+	public java.lang.Long getDim_levels__level_1() {
+		return this.dim_levels__level_1;
+	}
+
+	public void setDim_levels__level_1(java.lang.Long dim_levels__level_1) {
+		this.dim_levels__level_1 = dim_levels__level_1;
+	}
+
+	public java.lang.Long getDim_levels__level_2() {
+		return this.dim_levels__level_2;
+	}
+
+	public void setDim_levels__level_2(java.lang.Long dim_levels__level_2) {
+		this.dim_levels__level_2 = dim_levels__level_2;
+	}
+
+	public java.lang.Long getDim_levels__level_3() {
+		return this.dim_levels__level_3;
+	}
+
+	public void setDim_levels__level_3(java.lang.Long dim_levels__level_3) {
+		this.dim_levels__level_3 = dim_levels__level_3;
+	}
+
+	public java.lang.Long getDim_levels__level_4() {
+		return this.dim_levels__level_4;
+	}
+
+	public void setDim_levels__level_4(java.lang.Long dim_levels__level_4) {
+		this.dim_levels__level_4 = dim_levels__level_4;
+	}
+
+	public java.lang.Long getDim_levels__level_5() {
+		return this.dim_levels__level_5;
+	}
+
+	public void setDim_levels__level_5(java.lang.Long dim_levels__level_5) {
+		this.dim_levels__level_5 = dim_levels__level_5;
+	}
+
+	public java.lang.Long getDim_levels__level_6() {
+		return this.dim_levels__level_6;
+	}
+
+	public void setDim_levels__level_6(java.lang.Long dim_levels__level_6) {
+		this.dim_levels__level_6 = dim_levels__level_6;
+	}
+
+	public java.lang.Long getDim_levels__level_7() {
+		return this.dim_levels__level_7;
+	}
+
+	public void setDim_levels__level_7(java.lang.Long dim_levels__level_7) {
+		this.dim_levels__level_7 = dim_levels__level_7;
+	}
+
+	public java.lang.Long getDim_levels__level_8() {
+		return this.dim_levels__level_8;
+	}
+
+	public void setDim_levels__level_8(java.lang.Long dim_levels__level_8) {
+		this.dim_levels__level_8 = dim_levels__level_8;
+	}
+
+	public java.lang.Long getDim_levels__level_9() {
+		return this.dim_levels__level_9;
+	}
+
+	public void setDim_levels__level_9(java.lang.Long dim_levels__level_9) {
+		this.dim_levels__level_9 = dim_levels__level_9;
+	}
+
+	public data_bus_dim(java.lang.String dim_name,
+			java.lang.Long dim_levels__level_0,
+			java.lang.Long dim_levels__level_1,
+			java.lang.Long dim_levels__level_2,
+			java.lang.Long dim_levels__level_3,
+			java.lang.Long dim_levels__level_4,
+			java.lang.Long dim_levels__level_5,
+			java.lang.Long dim_levels__level_6,
+			java.lang.Long dim_levels__level_7,
+			java.lang.Long dim_levels__level_8,
+			java.lang.Long dim_levels__level_9) {
+		this.dim_name = dim_name;
+		this.dim_levels__level_0 = dim_levels__level_0;
+		this.dim_levels__level_1 = dim_levels__level_1;
+		this.dim_levels__level_2 = dim_levels__level_2;
+		this.dim_levels__level_3 = dim_levels__level_3;
+		this.dim_levels__level_4 = dim_levels__level_4;
+		this.dim_levels__level_5 = dim_levels__level_5;
+		this.dim_levels__level_6 = dim_levels__level_6;
+		this.dim_levels__level_7 = dim_levels__level_7;
+		this.dim_levels__level_8 = dim_levels__level_8;
+		this.dim_levels__level_9 = dim_levels__level_9;
+	}
 
 }
